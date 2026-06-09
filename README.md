1a12e8239fd0b520904141ae40daf79bd6bc4472
devasolutions-devos <4tysdead@gmail.com>
2026-06-09 11:07:17 -0600

    finished windows game

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # Beach Ball Bounce
 
-This is the repository for the code of "Brach Ball Bounce" a game featured on the [Comfy Jam: Summer 2026](https://itch.io/jam/comfy-jam-summer-2026) game jam. This game was made using Phaser.JS v4 and Packaged with Electron JS as a Windows executable.
+This is the repository for the code of "Brach Ball Bounce" a game featured on the [Comfy Jam: Summer 2026](https://itch.io/jam/comfy-jam-summer-2026) game jam. This game was made using Phaser v4.1.0 and packaged with Electron JS as a Windows executable.
 
 ## Credits
 
-Main developer: Mariano Hurtado de Mendoza Carranza [devasolutions-devos](https://github.com/devasolutions-devos)
-Assets: Beach ball assets by [ph0nsy](https://ph0nsy.itch.io/)
+I'm dot4 creator of [casa4studio](https://casa4studio.itch.io) and [devasolutions-devos](https://github.com/devasolutions-devos)
+
+Made with [Phaser v4.1.0](https://github.com/phaserjs/phaser) created by Richard Davey [photonstorm](https://x.com/photonstorm)
+
+Beach Ball 64x64 made by [ph0nsy](https://ph0nsy.itch.io)
+
+Comfy Summer 2026 Background made by dot4 @ [casa4studio](https://casa4studio.itch.io)
+
+Prelude and Bouncing Beach Ball (Original Beach Ball Bounce Soundtrack) made by dot4 @ [casa4studio](https://casa4studio.itch.io) and available on [SoundCloud](https://soundcloud.com/casa4studio) too
+
+This game code is available on [Github](https://github.com/devasolutions-devos/beach-ball-bounce) under the **Apache 2.0 License**
+
+Thank you Macrow for the opportunity in this Game Jam :)
 
 ## License
 
-This repository is under the **Apache License. 2.0** Any use of this code has to follow the guidelines of this license.
+This repository is under the **Apache License 2.0.** Any use of this code has to follow the guidelines of this license.
--- src/ControlsScene.ts
+++ src/ControlsScene.ts
@@ -1,47 +1,58 @@
+//Import necessary dependencies
 import Phaser from "phaser";
 
+//Declare controls scene
 export class ControlsScene extends Phaser.Scene {
     constructor(){
-        super({key: "controls-scene"});
+        super({key: "controls-scene"}); //Set scene id
     }
 
     create() {
-        
+        //add background image
         this.add.image(0,0,"summer-background").setOrigin(0,0);
 
+        //create controls string
         const controlsString = "Left-click on the ball to make them bounce!\n" +
             "The more bounces the higher your score!\n" +
             "You have 1 ball so don't let it fall!\n";
         
+        //make controls string into phaser text
         const controlsText = this.add.text(30, 30, controlsString,  {
             fontFamily: "Unkempt Regular",
             fontSize: "32px",
             color: "#ffedd7"
         }).setStroke("#3b132b", 4).setPadding(16);
 
+        //add playbutton
         const playButton = this.add.image(this.scale.width / 2, controlsText.y + controlsText.height + 50 , "play-button");
         
+        //initialize button
         this.initializeButton(playButton, "main-scene");
 
+        //create back button
         const backText = this.add.text(this.scale.width / 2, this.scale.height - 30, "<- Back",{
             fontFamily: "Unkempt Bold",
             fontSize: "40px",
             color: "#fbc697"
         }).setStroke("#3b132b", 6).setOrigin(0.5,0.5);
         
+        //initialize button
         this.initializeButton(backText, "menu-scene");
     }
 
     private initializeButton(button: Phaser.GameObjects.GameObject, sceneId: string): void {
-            button.setInteractive();
-    
-            button.on("pointerdown", () => {
-                this.scene.start(sceneId);
-                if(button instanceof Phaser.GameObjects.Image &&
-                    (button as Phaser.GameObjects.Image).texture.key == "play-button"
-                ) {
-                    this.sound.stopAll();
-                }
-            });
-        }
+        //set button as interactive
+        button.setInteractive();
+
+        button.on("pointerdown", () => {
+            this.scene.start(sceneId); //Start scene
+
+            //check if button is play to stop sound (yes i know this violates OCP xd)
+            if(button instanceof Phaser.GameObjects.Image &&
+                (button as Phaser.GameObjects.Image).texture.key == "play-button"
+            ) {
+                this.sound.stopAll();
+            }
+        });
+    }
 }
